5dc7be706846d7e5a172d54f1d738d8cf2d6dce1
Arda Özarslan <ozarslanarda55@gmail.com>
2023-02-22 00:30:08 +0300

    started from scratch

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+	private Rigidbody rb;
+	private PlayerInputActions playerInputActions;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		playerInputActions = new PlayerInputActions();
+		playerInputActions.Player.Enable();
+		playerInputActions.Player.Jump.performed += Jump;
+		// playerInputActions.Player.Movement.performed += Movement;
+	}
+
+	private void FixedUpdate() {
+		Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+		float speed = 10f;
+		rb.AddForce(new Vector3(inputVector.x, 0f, inputVector.y) * speed, ForceMode.Force);
+	}
+
+	// private void Movement(InputAction.CallbackContext context)
+	// {
+	// 	Debug.Log(context);
+	// 	Vector2 inputVector = context.ReadValue<Vector2>();
+	// 	float speed = 5f;
+	// 	rb.AddForce(new Vector3(inputVector.x, 0f, inputVector.y) * speed, ForceMode.Force);
+	// }
+
+	public void Jump(InputAction.CallbackContext context)
+	{
+		Debug.Log(context);
+		Debug.Log("Jump!");
+		rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+	}
+}
